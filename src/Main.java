fa287a19dda9bd085eb3e011ba26a9dbce63c549
davidthomson1 <nfb11170@uni.strath.ac.uk>
2015-12-18 12:26:45 +0000

    tidying up all the classes1

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,6 @@ public class Main {
 
 	
 	public static void main(String[] args)  {
-//		IndexFiles index = new IndexFiles();
-//		SearchWindow gui1 = new SearchWindow();
-//		gui1.run();
-
 		new IndexFiles();
 		SearchGUI gui = new SearchGUI();
 
