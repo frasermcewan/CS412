b102aa0a9e3408d47927c3c59990d4b5e299948a
davidthomson1 <nfb11170@uni.strath.ac.uk>
2015-12-18 12:30:35 +0000

    tidyign3

--- src/model/SearchFiles.java
+++ src/model/SearchFiles.java
@@ -103,8 +103,6 @@ public class SearchFiles {
 		BufferedReader in = null;
 		in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
-		System.out.println("Searching for: " + query);
-		//System.out.println("Searching for: " + pq);
 		if (repeat > 0) {                           // repeat & time as benchmark
 			Date start = new Date();
 			for (int i = 0; i < repeat; i++) {
@@ -124,9 +122,9 @@ public class SearchFiles {
 		// Collect enough docs to show 5 pages
 		TopDocs results = searcher.search(query, 5 * hitsPerPage);
 				
-		//TopDocs results = searcher.search(bqb.build(), 5 * hitsPerPage);
 		if (results.totalHits==0){	
-			
+			System.out.println("Zero Result Checker");
+
 		}
 
 		System.out.println(results.totalHits);
@@ -160,15 +158,12 @@ public class SearchFiles {
 		ScoreDoc[] hits = results.scoreDocs;
 
 		int numTotalHits = results.totalHits;
-		//System.out.println(numTotalHits + " total matching documents");
 
 		int start = 0;
 		int end = Math.min(numTotalHits, hitsPerPage); // end is the number of hits per page
 
 		while (true) {
 			if (end > hits.length) {
-				//System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-				//System.out.println("Collect more (y/n) ?");
 				String line = in.readLine();
 				if (line.length() == 0 || line.charAt(0) == 'n') {
 					break;
@@ -187,19 +182,17 @@ public class SearchFiles {
 				String path = doc.get("path");
 
 				if (path != null) {
-					//System.out.println();
-					//System.out.println((i+1) + ". " + path);
 					String title = doc.get("title");
 					title = title+" || Term Frequency: " + termFrequency;
 					if (title != null) {
 						System.out.println("Title: " + doc.get("title"));
 
 					}
-					//          System.out.println(title + " - " + path);
+
 					result.put(title, path);
 
 				} else {
-					//          System.out.println((i+1) + ". " + "No path for this document");
+
 				}
 			}
 
@@ -221,32 +214,19 @@ public class SearchFiles {
 
 		// Collect enough docs to show 5 pages
 		TopDocs results = searcher.search(pq, 5 * hitsPerPage);
-		//TopDocs results = searcher.search(bqb.build(), 5 * hitsPerPage);
 		if (results.totalHits==0){
-			
-			
 			System.out.println("Zero Result Checker");
-
-		}
-
-		//System.out.println(results2);
-
-		for (int i=0;i<results.scoreDocs.length;i++){
-			//	System.out.println(i+1 + ": " + results.scoreDocs[i]);
 		}
 
 		ScoreDoc[] hits = results.scoreDocs;
 
 		int numTotalHits = results.totalHits;
-		//System.out.println(numTotalHits + " total matching documents");
 
 		int start = 0;
 		int end = Math.min(numTotalHits, hitsPerPage); // end is the number of hits per page
 
 		while (true) {
 			if (end > hits.length) {
-				//System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-				//System.out.println("Collect more (y/n) ?");
 				String line = in.readLine();
 				if (line.length() == 0 || line.charAt(0) == 'n') {
 					break;
@@ -261,58 +241,17 @@ public class SearchFiles {
 
 				Document doc = searcher.doc(hits[i].doc);
 
-				String actualQueryString = query.toString().substring(9, query.toString().length());
-				int sizeOfActualQueryString = actualQueryString.length();
-
-				String termBeingSearched = doc.get("contents");
-
-
-				for (int j = -1; (j = termBeingSearched.indexOf(actualQueryString, j + 1)) != -1; ) {
-
-					// need to check the term is within the +25 and -25 otherwise it's out of bounds
-					// can fix this later on
-
-					//System.out.println(termBeingSearched);
-					int startIndexOfTermBeingSearched = j;
-					//System.out.println(startIndexOfTermBeingSearched);
-					int endIndexOfTermBeingSearched = startIndexOfTermBeingSearched+sizeOfActualQueryString;
-					//System.out.println(endIndexOfTermBeingSearched);
-
-					if (startIndexOfTermBeingSearched<25){
-						startIndexOfTermBeingSearched = 0;
-					} else {
-						startIndexOfTermBeingSearched=startIndexOfTermBeingSearched-25;
-					}
-					int temp = endIndexOfTermBeingSearched+25;
-					int termLength = termBeingSearched.length();
-					if (temp>=termLength){
-						endIndexOfTermBeingSearched=termLength;
-					} else {
-						endIndexOfTermBeingSearched=endIndexOfTermBeingSearched+25;
-					}
-
-					String trial = termBeingSearched.substring(startIndexOfTermBeingSearched, endIndexOfTermBeingSearched);
-					//System.out.println();
-					//System.out.println("term in context of the page:");
-					//System.out.println("... \" " + trial + " \"...");
-
-				}
-
+				
 				String path = doc.get("path");
 
 				if (path != null) {
-					//System.out.println();
-					//System.out.println((i+1) + ". " + path);
 					String title = doc.get("title");
 					if (title != null) {
-						//System.out.println("Title: " + doc.get("title"));
-
+						
 					}
-					//          System.out.println(title + " - " + path);
 					result.put(title, path);
 
 				} else {
-					//          System.out.println((i+1) + ". " + "No path for this document");
 				}
 			}
 
